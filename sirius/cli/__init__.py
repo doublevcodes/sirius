d41c136869e2042a1ee7eda2267d52510cecdf48
Shivansh-007 <shivansh-007@outlook.com>
2022-02-03 12:49:16 +0530

    feat: Add command to generate default config in CWD

--- sirius/cli/__init__.py
+++ sirius/cli/__init__.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+
 import click
 import uvicorn
 
 from sirius import __version__
+from sirius.config.config import DEFAULT_CONFIG_FILE_PATH
+from sirius.config.export import export_default_config
 
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]})
@@ -11,8 +15,23 @@ def main():
 
 
 @main.command()
-@click.option("-p", "--port", help="Port to run the server on")
-def dev(args):
-    uvicorn.run(
-        "sirius.sirius:sirius", port=int(args.port) if args.port else 8000, reload=True
+@click.argument(
+    "file_name",
+    default=DEFAULT_CONFIG_FILE_PATH,
+    type=click.Path(
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        writable=True,
+        allow_dash=False,
+        path_type=str,
     )
+)
+def export(file_name: str):
+    export_default_config(Path(file_name))
+
+
+@main.command()
+@click.option("-p", "--port", help="Port to run the server on")
+def dev(port: int):
+    uvicorn.run("sirius.sirius:sirius", port=port, reload=True)
